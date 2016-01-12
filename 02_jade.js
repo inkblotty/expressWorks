a16990a54f2d4a34d72019b5e143cbed5d4a46e3
inkblotty <inkblot.and.blush@gmail.com>
2016-01-12 15:23:31 -0700

    installed more dev dependencies

--- 02_jade.js
+++ 02_jade.js
@@ -5,7 +5,8 @@ var app = express();
 var port = process.argv[2];
 var file = process.argv[3];
 
-app.set('view engine', 'jade');
+app.set('view engine', 'jade'); // view engine is the alias of jade
+app.set('views', file); // views is the alias of the file passed to us
 
 app.get('/home', function(req, res) {
 	res.render(file, {date: new Date().toDateString()});
